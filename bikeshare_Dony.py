4eac4765ee26504b30b276c1d66fe11d7280e5c4
DonyCat <sgonza5369@gmail.com>
2022-09-30 22:19:02 -0600

    Modify wording to survey intro

--- bikeshare_Dony.py
+++ bikeshare_Dony.py
@@ -21,7 +21,7 @@ def get_filters():
     (str) month - name of the month to filter by, or "all" to apply no month filter
     (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s review data on bikeshare from 3 different cities!')
+    print('Hello! Let\'s review data on bikeshare from our 3 different cities!')
     # TO DO: get user input for city (chicago, new york, washington). HINT: Use a while loop to handle invalid inputs
     while True: 
       city = input('Which city would you like to explore? chicago, new york city, washington? \n> ').lower()
